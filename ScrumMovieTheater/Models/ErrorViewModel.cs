f0025265ee24bced5eaf5c68553774b71287faa1
strikeouts27 <99090208+strikeouts27@users.noreply.github.com>
2026-05-28 18:15:37 -0500

    updated template to mvc

---
+++ ScrumMovieTheater/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ScrumMovieTheater.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
